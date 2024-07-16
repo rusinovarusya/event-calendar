ae07532410f6b1ef8c9e5cc75bd6e09071a95514
Irina Rusinova <irina.rusinova19@gmail.com>
2024-07-16 11:27:03 +0300

    fix: change deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'event-calendar',
+  base: process.env.NODE_ENV === 'production' ? '/event-calendar/' : '/',
   plugins: [
     react(),
     createSvgIconsPlugin({
